b5c0ac689962a3c8768782eb4eb0ef1077fdc285
FzTeis <146758681+FzTeis@users.noreply.github.com>
2023-10-12 23:08:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('The Mystic Bot', {
+say('Simple | Wa - Bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`BrunoSobrino`, {
+say(`FzTeis`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
